264663ee3e43249b4970a539785860c75af4663b
praveen-kishore-pragya <praveen.pragya1@gmail.com>
2024-03-07 00:26:36 +0100

    Added firing ability in 2D_Shoot'em_Up game
    Thinking to apply threads to movement of bullets

--- 2D_Shoot'em_Up/2dShooter.cpp
+++ 2D_Shoot'em_Up/2dShooter.cpp
@@ -4,6 +4,9 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+//creeating thread to maintain movement of fired bullet on another thread,
+//bcoz the movemnt of thread will be in a loop, and it will hinder other activities of the main thread
+#include <thread>
 
 #define SCREEN_HEIGHT 720
 #define SCREEN_WIDTH 1280
@@ -16,6 +19,12 @@
 void printError()
 {
 	std::cout << " Error occured: " << SDL_GetError() << std::endl;
+}
+
+void checkPointer(void* ptr)
+{
+	if(ptr == nullptr)
+		printError();
 }
 
 int init()
@@ -45,32 +54,44 @@ SDL_Texture* createTexture(SDL_Renderer* renderer, std::string& filePath)
 }
 
 
-void renderScreen(SDL_Renderer* renderer, SDL_Texture* texture, int x = 0, int y = 0, int w = SCREEN_WIDTH, int h = SCREEN_HEIGHT, int delay = 1000)
+void renderPolice(SDL_Renderer* renderer, SDL_Texture* texturePolice,  SDL_Texture* textureBullet, int x = 0, int y = 0, int w = SCREEN_WIDTH, int h = SCREEN_HEIGHT, int delay = 1000, bool fire = false)
 {
-    SDL_Rect destination;
-    destination.x = x;
-    destination.y = y;
-    destination.w = w;
-    destination.h = h;
+	//creating police
+    SDL_Rect police;
+    police.x = x;
+    police.y = y;
+    police.w = w;
+    police.h = h;
 
 	
-	
-    
 	SDL_RenderClear(renderer);
 
 
-
-    SDL_RenderCopy(renderer, texture, NULL, &destination);
+    SDL_RenderCopy(renderer, texturePolice, NULL, &police);
 	
+	if(fire == true)
+	{
+		//creating bullet
+		//height and width of bullet is hard coded to be 10 each
+		SDL_Rect bullet;
+		bullet.x = police.x + w;
+		bullet.y = police.y + police.h/2;
+		bullet.w = 10;
+		bullet.h = 10;
+
+    	SDL_RenderCopy(renderer, textureBullet, NULL, &bullet);
+	}
+
 	SDL_RenderPresent(renderer);
 
     SDL_Delay(delay);
 
 }
 
-void cleanup(SDL_Renderer* renderer, SDL_Texture* texture, SDL_Window* window)
+void cleanup(SDL_Renderer* renderer, SDL_Texture* texturePolice, SDL_Texture* textureBullet, SDL_Window* window)
 {
-    SDL_DestroyTexture(texture);
+    SDL_DestroyTexture(texturePolice);
+    SDL_DestroyTexture(textureBullet);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 }
@@ -88,7 +109,7 @@ int main(int argc, char* argv[])
                                             SDL_WINDOWPOS_UNDEFINED,
                                             SCREEN_WIDTH,
                                             SCREEN_HEIGHT,
-                                            SDL_WINDOW_SHOWN);
+                                            SDL_WINDOW_SHOWN);  //can put  | SDL_WINDOW_RESIZABLE, BUT then have to calculate the resized window for character movement
     
     
 
@@ -103,23 +124,34 @@ int main(int argc, char* argv[])
 	SDL_SetRenderDrawColor(renderer, 47, 77, 76, 255);
 
     
-	std::string filePath = "./../resources/shoes.jpg";
+	std::string policePath = "./../resources/characters/Police.png";
+	std::string bulletPath = "./../resources/props/bullet.png";
+	std::string bulletShotPath = "./../resources/props/bullet_shot.png";
     
     
-    SDL_Texture* texture = createTexture(renderer, filePath);
-    if(texture == nullptr)
-    {
-        std::cout << " Texture was not created: " << SDL_GetError() << std::endl;
-        SDL_Quit();
+    SDL_Texture* texturePolice = createTexture(renderer, policePath);
+	checkPointer(texturePolice);
 
-        return 1;
-    }
+    SDL_Texture* textureBullet = createTexture(renderer, bulletPath);
+	checkPointer(textureBullet);
+    // if(texture == nullptr)
+    // {
+    //     std::cout << " Texture was not created: " << SDL_GetError() << std::endl;
+    //     SDL_Quit();
+
+    //     return 1;
+    // }
 
 	SDL_Event event;
 	bool quit = false;
-	//initial position of the ship/shooter
-	int directionX = 100;
-	int directionY = 100;
+
+	//keep track of firing bullets
+	bool fire = false;
+	
+	//initial position of the Police
+	int policeX = 100;
+	int policeY = 100;
+
 	while( quit != true)
 	{
 		while(SDL_PollEvent(&event))
@@ -133,35 +165,45 @@ int main(int argc, char* argv[])
 					case SDLK_ESCAPE : quit = true;
 										break;
 
-					case SDLK_UP : directionY -= 5;
+					case SDLK_UP : policeY -= 5;
 									//handling outbounds condition
-									directionY = directionY < 0 ? directionY + 5 : directionY;
+									policeY = policeY < 0 ? policeY + 5 : policeY;
 										break;
 
-					case SDLK_DOWN : directionY += 5;
+					case SDLK_DOWN : policeY += 5;
 									//handling outbounds condition
 									//NOTE : (SCREEN_HEIGHT - IMAGE_HEIGHT) checks that image stops fully before its front touches the boundary
-									directionY = directionY > (SCREEN_HEIGHT - IMAGE_HEIGHT) ? directionY - 5 : directionY;
+									policeY = policeY > (SCREEN_HEIGHT - IMAGE_HEIGHT) ? policeY - 5 : policeY;
 										break;
 
-					case SDLK_LEFT : directionX -= 5;
+					case SDLK_LEFT : policeX -= 5;
 									//handling outbounds condition
-									directionX = directionX < 0 ? directionX + 5 : directionX;
+									policeX = policeX < 0 ? policeX + 5 : policeX;
 										break;
 
-					case SDLK_RIGHT : directionX += 5;
+					case SDLK_RIGHT : policeX += 5;
 									//handling outbounds condition
 									//NOTE : (SCREEN_HEIGHT - IMAGE_WIDTH) checks that image stops fully before its front touches the boundary
-									directionX = directionX > (SCREEN_WIDTH - IMAGE_WIDTH) ? directionX - 5 : directionX;
+									policeX = policeX > (SCREEN_WIDTH - IMAGE_WIDTH) ? policeX - 5 : policeX;
 										break;
-					
+
 							default :
 										break;
 				}
+
+				if(event.key.keysym.sym == SDLK_SPACE)
+				{
+					fire = true;
+
+				}
 			}
+			
+
+
 		}
-		//render screen
-		renderScreen(renderer, texture, directionX, directionY, 100, 100, 16);
+		//render police
+		//height and width is kept 100 and 100 for the police
+		std::thread threadMainRender(renderPolice, renderer, texturePolice, textureBullet, policeX, policeY, 100, 100, 16, fire);
 
 	}
 
